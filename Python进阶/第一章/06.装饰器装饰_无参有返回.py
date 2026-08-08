071152d411ddb141546b05067c40ae7d4d333e6a
joeytang23 <3251734073@qq.com>
2026-08-08 21:54:59 +0800

    装饰器

---
+++ Python进阶/第一章/06.装饰器装饰_无参有返回.py
@@ -0,0 +1,27 @@
+"""
+    装饰器的内部函数格式要和被装饰的原函数保持一致
+    即原函数无参无返回,则内部函数必须也是
+    同理
+"""
+
+#需求:无参有返回值的getsum求和函数,不改变功能的基础上,添加友好提示
+#定义装饰器
+def my_decorator(fn_name):
+    def fn_inner():
+        print("正在努力计算中....")
+        return fn_name()
+    return fn_inner
+
+@my_decorator
+def getsum():
+    a=10
+    b=20
+    sum=a+b
+    return sum
+
+getsum()
+print("-------------------------")
+print(getsum())
+
+# getsum=my_decorator(getsum)
+# getsum()
